a98c1db12952559547e9e6d3fe78b8bbe3804a04
Abida634 <rehmanabida634@gmail.com>
2025-12-22 21:56:03 +0530

    Updated task2

--- task2.py
+++ task2.py
@@ -1,3 +1,4 @@
+
 # Take name from user
 first_name = input("Enter your first name: ")
 last_name = input("Enter your last name: ")
@@ -5,10 +6,4 @@ last_name = input("Enter your last name: ")
 # Print greeting
 print("Hello", first_name, last_name)
 print("Welcome to the Python program.")
-
-
-
-
-
-
 
