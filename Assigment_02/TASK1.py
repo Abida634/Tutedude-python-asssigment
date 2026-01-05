f81fc20dcdb3c7c11743ff47aefaaf9182185f92
Abida634 <rehmanabida634@gmail.com>
2026-01-05 10:30:30 +0530

    Added Task 1 and Task 2 Python files

---
+++ Assigment_02/TASK1.py
@@ -0,0 +1,12 @@
+# Task1
+# Check if a number is even or odd
+
+
+# Take input from the user
+num = int(input("Enter a number: "))
+
+# Check even or odd
+if num % 2 == 0:
+    print(num, "is an even number.")
+else:
+    print(num, "is an odd number.")
---
+++ Assigment_02/TASK2.py
@@ -0,0 +1,12 @@
+# Task 2
+# Sum of integers from 1 to 50 using a lool
+
+# Initialize sum variable
+total = 0
+
+# Loop from 1 to 50
+for i in range(1, 51):
+    total = total + i
+
+# Display the result
+print("The sum of numbers from 1 to 50 is:", total)
